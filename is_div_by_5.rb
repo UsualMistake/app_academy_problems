6432968dc28b536010bf7200b8f504d69f71da5a
Nelson A. J <51727175+UsualMistake@users.noreply.github.com>
2019-11-18 17:44:17 -0800

    Update is_div_dy_5

--- is_div_by_5.rb
+++ is_div_by_5.rb
@@ -1,3 +1,9 @@
+# Write a method is_div_by_5(number) 
+# that takes in a number and returns the boolean true
+# if the given number is divisible by 5, false otherwise
+
+
+# Solution
 def is_div_by_5(number)
 
   if(number % 5 == 0)
